7963c3e7f9698178e1fea2a52b5f3e4d68f6857f
moonwave99 <diego.caponera@gmail.com>
2015-05-07 17:04:13 +0200

    Update discogs icon.

--- discogs-embed.js
+++ discogs-embed.js
@@ -16,7 +16,8 @@
     imageCacheUrl: '/images/discogs',
     ui: {
       collapseThreshold: 10
-    }
+    },
+    discogsIcon: 'http://static.discogs.com/images/favicon.ico'
   };
   
   defaultDiscogsOptions = {
@@ -61,7 +62,7 @@
       var _this = this;
       this._done = function(result, status, jqXHR){
         result.data.image = _this.imageCacheUrl + '/' + result.data.id + '.jpeg';
-        _this.handler.toggleClass('error', false).html(_this.templates.main({ data : result.data })).addClass('loaded');
+        _this.handler.toggleClass('error', false).html(_this.templates.main({ data : result.data, icon: options.discogsIcon })).addClass('loaded');
       }
       
       this._fail = function(){
